c443284445e07c46f95fe65a3403ca3defd02991
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-07-13 14:28:32 +0330

    Add transaction to set the species column to 'unspecified' then rollback.

--- queries.sql
+++ queries.sql
@@ -8,3 +8,22 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-------------------------------------------------------------------------------------
+
+BEGIN; -- Start the transaction
+
+-- Update the species column to 'unspecified'
+UPDATE animals
+SET species = 'unspecified';
+
+-- Verify the change
+SELECT * FROM animals;
+
+-- Roll back the transaction
+ROLLBACK;
+
+-- Verify that the species column has been rolled back to the previous state
+SELECT * FROM animals;
+
+
