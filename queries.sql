fc7b673858aff86a35cf54e00e077117e9dc6f52
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-07-13 06:29:18 +0330

    Update queries.sql TO ADD all queries.

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
-/*Queries that provide answers to the questions from all projects.*/
+/*Queries that provide answers to the questions.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE neutered = true;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
