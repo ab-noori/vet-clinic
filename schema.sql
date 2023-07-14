2ebab271f47b017ecc337c2b5a0ec9d45c3feec6
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-07-14 18:07:36 +0330

    Modify schema to add vets table, specializations join table, and visits join table.

--- schema.sql
+++ schema.sql
@@ -38,3 +38,25 @@ ALTER TABLE animals
 ADD COLUMN species_id INTEGER REFERENCES species(id),
 ADD COLUMN owner_id INTEGER REFERENCES owners(id);
 
+-- Create the vets table
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+-- Create the specializations join table
+CREATE TABLE specializations (
+  vet_id INTEGER REFERENCES vets(id),
+  species_id INTEGER REFERENCES species(id),
+  PRIMARY KEY (vet_id, species_id)
+);
+
+-- Create the visits join table
+CREATE TABLE visits (
+  animal_id INTEGER REFERENCES animals(id),
+  vet_id INTEGER REFERENCES vets(id),
+  visit_date DATE,
+  PRIMARY KEY (animal_id, vet_id, visit_date)
+);
